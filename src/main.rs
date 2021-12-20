df76f08a6fea63ad041dab34e852702187f6f556
Aevyrie <aevyrie@gmail.com>
2021-12-20 01:18:28 -0800

    Tidy up code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use bevy::{
-    diagnostic::FrameTimeDiagnosticsPlugin,
     prelude::*,
     render::{RenderApp, RenderStage},
 };
@@ -19,31 +18,23 @@ fn main() {
     .add_plugin(DebugCursorPickingPlugin)
     .add_plugin(DebugEventsPickingPlugin)
     .add_startup_system(setup);
-    //.add_startup_system(setup_ui);
 
     let render_app = app.sub_app(RenderApp);
     render_app
         .insert_resource(FrameTimer::default())
         .add_system_to_stage(RenderStage::Render, framerate_exact_limiter)
         .add_system_to_stage(RenderStage::Cleanup, framerate_limit_forward_estimator);
-    //.add_system_to_stage(RenderStage::Cleanup, limiter_state);
-    app.run();
-}
 
-#[derive(SystemLabel, Clone, Hash, Debug, Eq, PartialEq)]
-enum FrameLimit {
-    Estimate,
-    Exact,
+    app.run();
 }
 
 #[derive(Debug)]
 struct FrameTimer {
     enabled: bool,
+    framerate_target: u64,
     frame_start: std::time::Instant,
-    exact_start: std::time::Instant,
     render_start: std::time::Instant,
     exact_sleep: std::time::Duration,
-    framerate_target: u64,
     safety_margin: std::time::Duration,
 }
 impl Default for FrameTimer {
@@ -52,7 +43,6 @@ impl Default for FrameTimer {
             enabled: true,
             frame_start: std::time::Instant::now(),
             render_start: std::time::Instant::now(),
-            exact_start: std::time::Instant::now(),
             exact_sleep: std::time::Duration::from_millis(0),
             framerate_target: 60,
             safety_margin: std::time::Duration::from_micros(500),
@@ -72,27 +62,18 @@ fn framerate_limit_forward_estimator(mut timer: ResMut<FrameTimer>) {
         spin_sleep::sleep(estimated_sleep_time);
     }
     timer.frame_start = std::time::Instant::now();
-    /*
-    info!(
-        "FT: {:>5.2}RT: {:>5.2}ms, EST: {:>5.2}ms, EXT: {:>5.2}ms",
-        last_frametime.as_micros() as f32 / 1000.,
-        last_render_time.as_micros() as f32 / 1000.,
-        estimated_sleep_time.as_micros() as f32 / 1000.,
-        timer.exact_sleep.as_micros() as f32 / 1000.,
-    );
-    */
 }
 
 fn framerate_exact_limiter(mut timer: ResMut<FrameTimer>) {
     let system_start = std::time::Instant::now();
     let target_frametime = std::time::Duration::from_micros(1_000_000 / timer.framerate_target);
     let sleep_needed =
-        target_frametime - target_frametime.min(system_start.duration_since(timer.exact_start));
+        target_frametime - target_frametime.min(system_start.duration_since(timer.render_start));
     if timer.enabled {
         spin_sleep::sleep(sleep_needed);
     }
-    timer.exact_start = std::time::Instant::now();
-    timer.exact_sleep = timer.exact_start.duration_since(system_start);
+    timer.render_start = std::time::Instant::now();
+    timer.exact_sleep = timer.render_start.duration_since(system_start);
 }
 
 /// set up a simple 3D scene
@@ -135,76 +116,4 @@ fn setup(
             ..Default::default()
         })
         .insert_bundle(PickingCameraBundle::default());
-}
-
-/*fn limiter_state(
-    mut timer: ResMut<FrameTimer>,
-    mut status_query: Query<&mut Text, With<StatusText>>,
-    keyboard: Res<Input<KeyCode>>,
-) {
-    let mut text = if let Ok(text) = status_query.get_single_mut() {
-        text
-    } else {
-        return;
-    };
-    if keyboard.just_pressed(KeyCode::Space) {
-        if timer.enabled {
-            timer.enabled = false;
-            text.sections[0].value = "OFF".to_string();
-            text.sections[0].style.color = Color::RED;
-        } else {
-            timer.enabled = true;
-            text.sections[0].value = "ON".to_string();
-            text.sections[0].style.color = Color::GREEN;
-        }
-    }
-}*/
-
-#[derive(Component)]
-struct StatusText;
-
-fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(UiCameraBundle::default());
-    let font = asset_server.load("fonts/FiraMono-Medium.ttf");
-    commands
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                align_self: AlignSelf::FlexStart,
-                flex_direction: FlexDirection::Column,
-                ..Default::default()
-            },
-            color: Color::NONE.into(),
-            ..Default::default()
-        })
-        .with_children(|ui| {
-            ui.spawn_bundle(TextBundle {
-                text: Text {
-                    sections: vec![TextSection {
-                        value: "Press spacebar to toggle".to_string(),
-                        style: TextStyle {
-                            font: font.clone(),
-                            font_size: 30.0,
-                            color: Color::WHITE,
-                        },
-                    }],
-                    ..Default::default()
-                },
-                ..Default::default()
-            });
-            ui.spawn_bundle(TextBundle {
-                text: Text {
-                    sections: vec![TextSection {
-                        value: "OFF".to_string(),
-                        style: TextStyle {
-                            font: font.clone(),
-                            font_size: 30.0,
-                            color: Color::RED,
-                        },
-                    }],
-                    ..Default::default()
-                },
-                ..Default::default()
-            })
-            .insert(StatusText);
-        });
 }
